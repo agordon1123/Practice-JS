af2553869972a825e8ed6137e53fddda9e4e0c9b
Alex <agordon1123@gmail.com>
2020-01-28 22:18:38 -0500

    completed queue and stack

--- Structures/Queue/index.js
+++ Structures/Queue/index.js
@@ -1,10 +1,23 @@
 function Queue() {
   this.size = 0;
-  // this.storage = ?
+  this.storage = [];
 }
 
-Queue.prototype.len = function() {};
-Queue.prototype.enqueue = function(value) {};
-Queue.prototype.dequeue = function() {};
+Queue.prototype.len = function() {
+  return this.size;
+};
+Queue.prototype.enqueue = function(value) {
+  this.storage.push(value);
+  this.size += 1
+};
+Queue.prototype.dequeue = function() {
+  if (this.size === 0) {
+    return null
+  } else {
+    x = this.storage.shift();
+    this.size -= 1
+    return x;
+  }
+};
 
 module.exports = { Queue };
--- Structures/Stack/index.js
+++ Structures/Stack/index.js
@@ -1,10 +1,23 @@
-function Stack() {
+function Stack(capacity=10) {
   this.size = 0;
-  // this.storage = ?
+  this.storage = [];
 }
 
-Stack.prototype.len = function() {};
-Stack.prototype.push = function(value) {};
-Stack.prototype.pop = function() {};
+Stack.prototype.len = function() {
+  return this.size
+};
+Stack.prototype.push = function(value) {
+  this.storage.push(value)
+  this.size += 1
+};
+Stack.prototype.pop = function() {
+  if (this.size > 0) {
+    x = this.storage.pop()
+    this.size -= 1
+    return x
+  } else {
+    return null
+  }
+};
 
 module.exports = { Stack };
